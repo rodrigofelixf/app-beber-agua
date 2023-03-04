2b4db416d134e8b9e3229bb5945eda05f9d63a35
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2023-03-04 18:55:17 -0300

    deploy

--- app/src/main/java/br/com/dio/appbeberagua/MainActivity.kt
+++ app/src/main/java/br/com/dio/appbeberagua/MainActivity.kt
@@ -1,13 +1,62 @@
 package br.com.dio.appbeberagua
 
+import android.app.AlertDialog
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import br.com.dio.appbeberagua.databinding.ActivityMainBinding
+import br.com.dio.appbeberagua.model.CalcularIngestaoDiaria
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         supportActionBar!!.hide()
     }
+
+    override fun onStart() {
+        super.onStart()
+        calcularAgua()
+        redefinirDados()
+    }
+
+
+    fun calcularAgua() {
+        binding.buttonCalcular.setOnClickListener {
+            if(editTextIsEmpty()) {
+                Toast.makeText(this, R.string.alerta_campo_vazio, Toast.LENGTH_SHORT).show()
+            } else {
+                val peso = binding.editPeso.text.toString().toDouble()
+                val idade = binding.editIdade.text.toString().toInt()
+                val resultadoMl = CalcularIngestaoDiaria.calcularTotalML(peso, idade).toInt()
+
+                binding.textResultadoMl.text = "${resultadoMl}ml"
+            }
+
+        }
+    }
+
+    fun redefinirDados() {
+        binding.icRedefinir.setOnClickListener {
+            AlertDialog.Builder(this)
+                .setTitle(R.string.dialog_titulo)
+                .setMessage(R.string.dialog_descricao)
+                .setPositiveButton("Ok") { _, _ ->
+                    binding.editPeso.text = null
+                    binding.editIdade.text = null
+                    binding.textResultadoMl.text = null
+                }
+                .setNegativeButton("Cancelar") { _, _ -> }
+                .show()
+        }
+    }
+
+    private fun editTextIsEmpty() : Boolean {
+        return (binding.editPeso.text.toString().isEmpty() || binding.editIdade.text.toString().isEmpty())
+    }
+
+
 }
---
+++ app/src/main/java/br/com/dio/appbeberagua/model/CalcularIngestaoDiaria.kt
@@ -0,0 +1,25 @@
+package br.com.dio.appbeberagua.model
+
+class CalcularIngestaoDiaria {
+
+    companion object {
+        private val ML_JOVEM = 40.0
+        private val ML_ADULTO = 35.0
+        private val ML_IDOSO = 30.0
+        private val ML_MAIS_DE_66_ANOS = 25.0
+
+        private var resultadoML = 0.0
+        private var resultado_total_ml = 0.0
+
+        fun calcularTotalML(peso: Double, idade: Int): Double {
+            val resultadoML: Double = when {
+                idade <= 17 -> peso * ML_JOVEM
+                idade <= 55 -> peso * ML_ADULTO
+                idade <= 65 -> peso * ML_IDOSO
+                else -> peso * ML_MAIS_DE_66_ANOS
+            }
+            return resultadoML
+        }
+    }
+
+}
